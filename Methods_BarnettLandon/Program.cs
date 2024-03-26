49be01a84b89a7ac1dd806d76a204f96e3e5124e
M1ST-hub <nmckrypt@gmail.com>
2024-03-26 09:55:23 -0500

    wuht

---
+++ Methods_BarnettLandon/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Methods_BarnettLandon
+{
+    internal class Program
+    {
+
+        //prints code to console and asks numbers/name
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a whole numba");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type anotha whole numba");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Mult(x,y));
+
+            Console.WriteLine("Wusss yo name");
+            string name = Console.ReadLine();
+
+            Howdy(name);
+
+            Cya();
+        }
+        //multiplies 2 user inputted numbers
+        static int Mult(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        //says hello
+        static void Howdy(string username)
+        {
+            Console.WriteLine($"Howdy , {username}! ");
+        }
+
+        //says goodbye
+        static void Cya()
+        {
+            Console.WriteLine("Have a lovely day <3");
+        }
+    }
+}
